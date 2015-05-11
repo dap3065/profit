62f350475be1f41f9d58e7cd9234b4bff0e23c0b
David Paul <dap3065@yahoo.com>
2015-05-11 10:49:42 -0500

    Changing about

--- frontend/views/site/about.php
+++ frontend/views/site/about.php
@@ -2,28 +2,23 @@
 use yii\helpers\Html;
 
 /* @var $this yii\web\View */
-$this->title = 'About';
+$this->title = 'About OG Profit K';
 $this->params['breadcrumbs'][] = $this->title;
 ?>
 <div class="site-about">
     <h1><?= Html::encode($this->title) ?></h1>
 
 <p>
-How your website functions is just as important as how it looks. Web development is a complex process involving many moving parts which must work together seamlessly. For projects to exceed your expectations and finish on time and within budget, we take the necessary time and listen intently to understand your goals and specific requirements. You will have real-time access to the plan and timeline mapped out in detail, so you never have to guess what progress is being made or what deadline is coming up next. Once the site begins to take shape, we go through several rounds of review to make sure you are thrilled with the result.
+Fort Worth, TX on the scene.  Get music instantly.  
 </p>
 <p>
-At Website Solutions Now, we believe websites with great development follow a few simple rules:
+Pictures created by Villian Music.
 </p>
 <p>
-    The code should be clean and lean, and should follow modern coding standards
-    The interface should be cross-browser compatible and should utilize the latest standards where appropriate (such as HTML5 and CSS 3.)
-    The website should be compatible with a variety of devices, including desktops, laptops, tablets and handhelds.
-    The site should integrate with a modern content management system or programming framework that will allow for future scalability
+Videos provided by youtube.
 </p>
 <p>
-Content Management System Integration</br>
-Ecommerce Development</br>
-Responsive Website Development</br>
+Contact Us 24/7 ... Grind never stops
 </p>
 <p>
 Contact us!
