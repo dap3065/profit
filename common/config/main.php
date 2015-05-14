39b3bdd4bb3fe37e863d30e26eca78910f8a5c02
David Paul <dap3065@yahoo.com>
2015-05-14 15:36:50 -0500

    Mail setup

--- common/config/main.php
+++ common/config/main.php
@@ -10,6 +10,14 @@ return [
         ],
         'mailer' => [
             'class' => 'yii\swiftmailer\Mailer',
+              'transport' => [
+            	'class' => 'Swift_SmtpTransport',
+             	'host' => 'smtp.gmail.com',  // e.g. smtp.mandrillapp.com or smtp.gmail.com
+             	'username' => 'david.paul.inc@gmail.com',
+             	'password' => 'Dp09556924',
+             	'port' => '587', // Port 25 is a very common port too
+             	'encryption' => 'tls', // It is often used, check your provider or mail server 
+               ],
         ],
 	'urlManager' => [
     		'enablePrettyUrl' => true,
